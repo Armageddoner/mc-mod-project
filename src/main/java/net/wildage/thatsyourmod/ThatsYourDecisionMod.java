327b44d466c6fae2c0b14a702763a590eef527e4
armageddoner19 <isaactipton125@gmail.com>
2026-02-17 10:09:49 -0500

    Added new item

--- src/main/java/net/wildage/thatsyourmod/ThatsYourDecisionMod.java
+++ src/main/java/net/wildage/thatsyourmod/ThatsYourDecisionMod.java
@@ -1,5 +1,6 @@
 package net.wildage.thatsyourmod;
 
+import net.wildage.thatsyourmod.item.ModItems;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -72,7 +73,7 @@ public class ThatsYourDecisionMod {
         // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
-
+        ModItems.register(modEventBus);
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -86,7 +87,9 @@ public class ThatsYourDecisionMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.COGITO);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/wildage/thatsyourmod/item/ModItems.java
@@ -0,0 +1,19 @@
+package net.wildage.thatsyourmod.item;
+
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+import net.wildage.thatsyourmod.ThatsYourDecisionMod;
+
+public class ModItems {
+
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(ThatsYourDecisionMod.MODID);
+
+    public static final DeferredItem<Item> COGITO = ITEMS.register("cogito", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
