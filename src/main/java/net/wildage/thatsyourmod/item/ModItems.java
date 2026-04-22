5b3d41fcadf86dfe85a6f3a80498d7d6a73325db
def4lt0 <russbug0418@gmail.com>
2026-04-22 13:59:08 -0400

    patched some armor registering bugs that broke gradle build

--- src/main/java/net/wildage/thatsyourmod/item/ModItems.java
+++ src/main/java/net/wildage/thatsyourmod/item/ModItems.java
@@ -14,10 +14,10 @@ public class ModItems {
     public static final DeferredItem<Item> COGITO = ITEMS.register("cogito", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> ENKEPHALIN = ITEMS.register("enkephalin", () -> new Item(new Item.Properties()));
 
-    public static final DeferredItem<Item> REGREIUM_HELMET = ITEMS.register("regreium_helmet", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM), ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)));
-    public static final DeferredItem<Item> REGREIUM_CHESTPLATE = ITEMS.register("regreium_chestplate", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM), ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)));
-    public static final DeferredItem<Item> REGREIUM_LEGGINGS = ITEMS.register("regreium_leggings", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM), ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)));
-    public static final DeferredItem<Item> REGREIUM_BOOTS = ITEMS.register("regreium_boots", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM), ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> REGREIUM_HELMET = ITEMS.register("regreium_helmet", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM.value()), ArmorItem.Type.HELMET, new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> REGREIUM_CHESTPLATE = ITEMS.register("regreium_chestplate", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM.value()), ArmorItem.Type.CHESTPLATE, new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> REGREIUM_LEGGINGS = ITEMS.register("regreium_leggings", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM.value()), ArmorItem.Type.LEGGINGS, new Item.Properties().stacksTo(1)));
+    public static final DeferredItem<Item> REGREIUM_BOOTS = ITEMS.register("regreium_boots", () -> new ArmorItem(Holder.direct(ModArmorMaterials.REGREIUM.value()), ArmorItem.Type.BOOTS, new Item.Properties().stacksTo(1)));
 
     public static final DeferredItem<Item> REGREIUM_PICKAXE = ITEMS.register("regreium_pickaxe", () -> new PickaxeItem(ModToolTiers.REGREIUM, new Item.Properties()));
     public static final DeferredItem<Item> REGREIUM_SWORD = ITEMS.register("regreium_sword", () -> new SwordItem(ModToolTiers.REGREIUM, new Item.Properties()));
